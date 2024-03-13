ac887bf2aeabf09162f2924f7bdfedd1e5caca24
itstotallyjan <jan.santi@icloud.com>
2024-03-13 10:09:01 -0400

    feat: vercel speed analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Analytics />
+      <SpeedInsights />
       <Component {...pageProps} />
       <Navbar />
       <Footer />
